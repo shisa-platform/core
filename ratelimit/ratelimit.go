4ab4eca10002d3f7636f4091ea28efaaafc28fb3
kevin birch <kmb@percolate.com>
2017-09-27 19:33:45 -0400

    use named return values

--- ratelimit/ratelimit.go
+++ ratelimit/ratelimit.go
@@ -14,34 +14,34 @@ type RateLimit struct {
 	Period time.Duration
 }
 
-func FromString(value string) (RateLimit, error) {
-	result := RateLimit{}
-
+func FromString(value string) (r RateLimit, err error) {
 	matches := limitRegex.FindAllStringSubmatch(value, 2)
 	if matches == nil || len(matches) == 0 || len(matches[0]) != 3 {
-		return result, fmt.Errorf("%q is not a valid rate limit expression", value)
+		err = fmt.Errorf("%q is not a valid rate limit expression", value)
+		return
 	}
 
 	limit, err := strconv.Atoi(matches[0][1])
 	if err != nil || 0 > limit {
-		return result, fmt.Errorf("%q is not a valid rate limit expression", value)
+		err = fmt.Errorf("%q is not a valid rate limit expression", value)
+		return
 	}
-	result.Limit = limit
+	r.Limit = limit
 
 	switch matches[0][2] {
 	case "d":
-		result.Period = time.Hour * 24
+		r.Period = time.Hour * 24
 	case "h":
-		result.Period = time.Hour
+		r.Period = time.Hour
 	case "m":
-		result.Period = time.Minute
+		r.Period = time.Minute
 	case "s":
-		result.Period = time.Second
+		r.Period = time.Second
 	default:
-		return result, fmt.Errorf("%q is not a valid rate limit expression", value)
+		err = fmt.Errorf("%q is not a valid rate limit expression", value)
 	}
 
-	return result, nil
+	return
 }
 
 func (r RateLimit) String() string {
