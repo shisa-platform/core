0dd9d59e8798e773e30d3eb9a1c8a9024c401912
kevin birch <kmb@percolate.com>
2017-12-04 16:33:33 -0500

    split http aux server into separate struct

--- auxillary/debug.go
+++ auxillary/debug.go
@@ -2,7 +2,6 @@ package auxillary
 
 import (
 	"context"
-	"crypto/tls"
 	"expvar"
 	"net/http"
 	"time"
@@ -15,72 +14,18 @@ const (
 	defaultDebugServerPath = "/debug/vars"
 )
 
-// xxx - accessory servers need pipelines as well (authn/authz)
-
 type DebugServer struct {
-	Path             string // URL path to listen on, "/debug/vars" if empty
-	Address          string // TCP address to listen on, ":http" if empty
-	UseTLS           bool   // should this server use TLS?
-	DisableKeepAlive bool   // Should TCP keep alive be disabled?
+	HTTPServer
+	Path string // URL path to listen on, "/debug/vars" if empty
 
-	TLSConfig *tls.Config // optional TLS config, used by ServeTLS and ListenAndServeTLS
-
-	// ReadTimeout is the maximum duration for reading the entire
-	// request, including the body.
-	//
-	// Because ReadTimeout does not let Handlers make per-request
-	// decisions on each request body's acceptable deadline or
-	// upload rate, most users will prefer to use
-	// ReadHeaderTimeout. It is valid to use them both.
-	ReadTimeout time.Duration
-
-	// ReadHeaderTimeout is the amount of time allowed to read
-	// request headers. The connection's read deadline is reset
-	// after reading the headers and the Handler can decide what
-	// is considered too slow for the body.
-	ReadHeaderTimeout time.Duration
-
-	// WriteTimeout is the maximum duration before timing out
-	// writes of the response. It is reset whenever a new
-	// request's header is read. Like ReadTimeout, it does not
-	// let Handlers make decisions on a per-request basis.
-	WriteTimeout time.Duration
-
-	// IdleTimeout is the maximum amount of time to wait for the
-	// next request when keep-alives are enabled. If IdleTimeout
-	// is zero, the value of ReadTimeout is used. If both are
-	// zero, ReadHeaderTimeout is used.
-	IdleTimeout time.Duration
-
-	// MaxHeaderBytes controls the maximum number of bytes the
-	// server will read parsing the request header's keys and
-	// values, including the request line. It does not limit the
-	// size of the request body.
-	// If zero, DefaultMaxHeaderBytes is used.
-	MaxHeaderBytes int
-
-	// TLSNextProto optionally specifies a function to take over
-	// ownership of the provided TLS connection when an NPN/ALPN
-	// protocol upgrade has occurred. The map key is the protocol
-	// name negotiated. The Handler argument should be used to
-	// handle HTTP requests and will initialize the Request's TLS
-	// and RemoteAddr if not already set. The connection is
-	// automatically closed when the function returns.
-	// If TLSNextProto is not nil, HTTP/2 support is not enabled
-	// automatically.
-	TLSNextProto map[string]func(*http.Server, *tls.Conn, http.Handler)
-
+	// Logger optionally specifies the logger to use by the
+	// Gateway and all of its services.  Leave this as nil to
+	// disable all logging.
 	Logger *zap.Logger
-
-	base http.Server
 }
 
 func (s *DebugServer) Name() string {
 	return "debug"
-}
-
-func (s *DebugServer) Addr() string {
-	return s.Address
 }
 
 func (s *DebugServer) Serve() error {
@@ -106,15 +51,15 @@ func (s *DebugServer) Serve() error {
 	mux.Handle(path, expvar.Handler())
 	s.base.Handler = mux
 
-	if s.UseTLS {
-		return s.base.ListenAndServeTLS("", "")
-	}
-
 	if s.Logger == nil {
 		s.Logger = zap.NewNop()
 	}
 
 	s.Logger.Info("starting debug server...", zap.String("addr", s.Address))
+
+	if s.UseTLS {
+		return s.base.ListenAndServeTLS("", "")
+	}
 
 	return s.base.ListenAndServe()
 }
