0ff6f654b50e7be60446c76b0d1a79a7c5f9dfec
kevin birch <kmb@percolate.com>
2017-12-19 16:41:27 -0500

    authentication middleware

---
+++ middleware/authentication.go
@@ -0,0 +1,80 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/ansel1/merry"
+
+	"github.com/percolate/shisa/authn"
+	"github.com/percolate/shisa/context"
+	"github.com/percolate/shisa/service"
+)
+
+var (
+	wwwAuthenticateHeaderKey = http.CanonicalHeaderKey("WWW-Authenticate")
+)
+
+// Authentication is middleware to help automate authentication.
+//
+// `Provider` must be non-nil or a InternalServiceError status
+// response will be returned.
+// `Challenge` should be provided and will be returned as the
+// value of the "WWW-Authenticate" response header if the default
+// handlers are invoked.
+// `UnauthorizedHandler` can be set to optionally customize the
+// response for an unknown user.  The default handler will
+// return a 401 status code, the "WWW-Authenticate" header and an
+// empty body.
+// `ErrorHandler` can be set to optionally customize the response
+// for an error. The `err` parameter passed to the handler will
+// have a recommended HTTP status code. The default handler will
+// return the recommended status code, the "WWW-Authenticate"
+// header and an empty body.
+type Authenticator struct {
+	Provider            authn.Provider
+	Challenge           string
+	UnauthorizedHandler service.Handler
+	ErrorHandler        service.ErrorHandler
+}
+
+func (m *Authenticator) Service(ctx context.Context, r *service.Request) service.Response {
+	if m.ErrorHandler == nil {
+		m.ErrorHandler = m.defaultErrorHandler
+	}
+	if m.UnauthorizedHandler == nil {
+		m.UnauthorizedHandler = m.defaultHandler
+	}
+
+	if m.Provider == nil {
+		err := merry.New("authn.Provider is nil")
+		err = err.WithUserMessage("Authenticator.Provider must be non-nil")
+		err = err.WithHTTPCode(http.StatusInternalServerError)
+		return m.ErrorHandler(ctx, r, err)
+	}
+
+	user, err := m.Provider.Authenticate(ctx, r)
+	if err != nil {
+		err = err.WithHTTPCode(http.StatusUnauthorized)
+		return m.ErrorHandler(ctx, r, err)
+	}
+	if user == nil {
+		return m.UnauthorizedHandler(ctx, r)
+	}
+
+	ctx = ctx.WithActor(user)
+	return nil
+}
+
+func (m *Authenticator) defaultHandler(ctx context.Context, r *service.Request) service.Response {
+	response := service.NewEmpty(http.StatusUnauthorized)
+	response.Headers().Set(wwwAuthenticateHeaderKey, m.Challenge)
+
+	return response
+}
+
+func (m *Authenticator) defaultErrorHandler(ctx context.Context, r *service.Request, err merry.Error) service.Response {
+	response := service.NewEmpty(merry.HTTPCode(err))
+	response.Headers().Set(wwwAuthenticateHeaderKey, m.Challenge)
+
+	return response
+}
---
+++ middleware/authentication_test.go
@@ -0,0 +1,183 @@
+package middleware
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/ansel1/merry"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/percolate/shisa/authn"
+	"github.com/percolate/shisa/context"
+	"github.com/percolate/shisa/models"
+	"github.com/percolate/shisa/service"
+)
+
+var (
+	expectedUser      = &models.FakeUser{IDHook: func() string { return "123" }}
+	fakeRequest       = httptest.NewRequest("GET", "/foo", nil)
+	expectedChallenge = "Brute realm=\"Outer Space\""
+)
+
+func TestNilProvider(t *testing.T) {
+	cut := &Authenticator{
+		Challenge: expectedChallenge,
+	}
+
+	request := &service.Request{Request: fakeRequest}
+	ctx := context.NewFakeContextDefaultFatal(t)
+	response := cut.Service(ctx, request)
+
+	assert.NotNil(t, response)
+	assert.Equal(t, http.StatusInternalServerError, response.StatusCode())
+	assert.Equal(t, expectedChallenge, response.Headers().Get(wwwAuthenticateHeaderKey))
+}
+
+func TestAuthnError(t *testing.T) {
+	authn := &authn.FakeProvider{
+		AuthenticateHook: func(context.Context, *service.Request) (models.User, merry.Error) {
+			return nil, merry.New("I blewed up!")
+		},
+	}
+
+	cut := &Authenticator{
+		Provider: authn,
+		Challenge: expectedChallenge,
+	}
+
+	request := &service.Request{Request: fakeRequest}
+	ctx := context.NewFakeContextDefaultFatal(t)
+
+	response := cut.Service(ctx, request)
+	assert.NotNil(t, response)
+	assert.Equal(t, http.StatusUnauthorized, response.StatusCode())
+	assert.Equal(t, expectedChallenge, response.Headers().Get(wwwAuthenticateHeaderKey))
+
+	authn.AssertAuthenticateCalledOnceWith(t, ctx, request)
+}
+
+func TestOK(t *testing.T) {
+	authn := &authn.FakeProvider{
+		AuthenticateHook: func(context.Context, *service.Request) (models.User, merry.Error) {
+			return expectedUser, nil
+		},
+	}
+
+	cut := &Authenticator{
+		Provider: authn,
+		Challenge: expectedChallenge,
+	}
+
+	request := &service.Request{Request: fakeRequest}
+	ctx := context.NewFakeContextDefaultFatal(t)
+	ctx.WithActorHook = func(value models.User) context.Context { return ctx }
+
+	response := cut.Service(ctx, request)
+	assert.Nil(t, response)
+	assert.NotNil(t, cut.ErrorHandler)
+
+	authn.AssertAuthenticateCalledOnceWith(t, ctx, request)
+	ctx.AssertWithActorCalledOnceWith(t, expectedUser)
+}
+
+func TestUnauthorized(t *testing.T) {
+	authn := &authn.FakeProvider{
+		AuthenticateHook: func(context.Context, *service.Request) (models.User, merry.Error) {
+			return nil, nil
+		},
+	}
+
+	cut := &Authenticator{
+		Provider:  authn,
+		Challenge: expectedChallenge,
+	}
+
+	request := &service.Request{Request: fakeRequest}
+	ctx := context.NewFakeContextDefaultFatal(t)
+
+	response := cut.Service(ctx, request)
+	assert.NotNil(t, response)
+	assert.Equal(t, http.StatusUnauthorized, response.StatusCode())
+	assert.Equal(t, expectedChallenge, response.Headers().Get(wwwAuthenticateHeaderKey))
+
+	authn.AssertAuthenticateCalledOnceWith(t, ctx, request)
+}
+
+func TestCustomHandler(t *testing.T) {
+	authn := &authn.FakeProvider{
+		AuthenticateHook: func(context.Context, *service.Request) (models.User, merry.Error) {
+			return nil, nil
+		},
+	}
+
+	request := &service.Request{Request: fakeRequest}
+	ctx := context.NewFakeContextDefaultFatal(t)
+
+	challenge := "Custom realm=\"secrets, inc\""
+	var handlerInvoked bool
+	cut := &Authenticator{
+		Provider: authn,
+		Challenge: expectedChallenge,
+		UnauthorizedHandler: func(c context.Context, r *service.Request) service.Response {
+			handlerInvoked = true
+			assert.Equal(t, ctx, c)
+			assert.Equal(t, request, r)
+
+			response := service.NewEmpty(http.StatusForbidden)
+			response.Headers().Set(wwwAuthenticateHeaderKey, challenge)
+			return response
+		},
+	}
+
+	response := cut.Service(ctx, request)
+	assert.NotNil(t, response)
+	assert.Equal(t, http.StatusForbidden, response.StatusCode())
+	assert.Equal(t, challenge, response.Headers().Get(wwwAuthenticateHeaderKey))
+
+	if !handlerInvoked {
+		t.Fatal("custom error handler not invoked")
+	}
+
+	authn.AssertAuthenticateCalledOnceWith(t, ctx, request)
+}
+
+func TestCustomErrorHandler(t *testing.T) {
+	authn := &authn.FakeProvider{
+		AuthenticateHook: func(context.Context, *service.Request) (models.User, merry.Error) {
+			return nil, merry.New("I blewed up!")
+		},
+	}
+
+	challenge := "Custom realm=\"secrets, inc\""
+	request := &service.Request{Request: fakeRequest}
+	ctx := context.NewFakeContextDefaultFatal(t)
+
+	var errorHandlerInvoked bool
+	cut := &Authenticator{
+		Provider: authn,
+		Challenge: expectedChallenge,
+		ErrorHandler: func(c context.Context, r *service.Request, err merry.Error) service.Response {
+			errorHandlerInvoked = true
+			assert.Equal(t, ctx, c)
+			assert.Equal(t, request, r)
+			assert.NotNil(t, err)
+			assert.Equal(t, http.StatusUnauthorized, merry.HTTPCode(err))
+
+			response := service.NewEmpty(http.StatusForbidden)
+			response.Headers().Set(wwwAuthenticateHeaderKey, challenge)
+			return response
+		},
+	}
+
+	response := cut.Service(ctx, request)
+	assert.NotNil(t, response)
+	assert.Equal(t, http.StatusForbidden, response.StatusCode())
+	assert.Equal(t, challenge, response.Headers().Get(wwwAuthenticateHeaderKey))
+
+	if !errorHandlerInvoked {
+		t.Fatal("custom error handler not invoked")
+	}
+
+	authn.AssertAuthenticateCalledOnceWith(t, ctx, request)
+}
