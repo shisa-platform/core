89e3e73877b64832a09be9d8421e6c2dcc569180
kevin birch <kmb@percolate.com>
2017-12-20 13:26:27 -0500

    add endpoint factories for simple case

--- service/endpoint.go
+++ service/endpoint.go
@@ -24,3 +24,98 @@ type Endpoint struct {
 	Trace   *Pipeline
 	// xxx - request (query|body) parameters
 }
+
+func GetEndpoint(route string, handlers ...Handler) Endpoint {
+	return Endpoint{
+		Route: route,
+		Get: &Pipeline{
+			Handlers: handlers,
+		},
+	}
+}
+
+func GetEndpointWithPolicy(route string, policy Policy, handlers ...Handler) Endpoint {
+	return Endpoint{
+		Route: route,
+		Get: &Pipeline{
+			Policy: policy,
+			Handlers: handlers,
+		},
+	}
+}
+
+func PutEndpoint(route string, handlers ...Handler) Endpoint {
+	return Endpoint{
+		Route: route,
+		Put: &Pipeline{
+			Handlers: handlers,
+		},
+	}
+}
+
+func PutEndpointWithPolicy(route string, policy Policy, handlers ...Handler) Endpoint {
+	return Endpoint{
+		Route: route,
+		Put: &Pipeline{
+			Policy: policy,
+			Handlers: handlers,
+		},
+	}
+}
+
+func PostEndpoint(route string, handlers ...Handler) Endpoint {
+	return Endpoint{
+		Route: route,
+		Post: &Pipeline{
+			Handlers: handlers,
+		},
+	}
+}
+
+func PostEndpointWithPolicy(route string, policy Policy, handlers ...Handler) Endpoint {
+	return Endpoint{
+		Route: route,
+		Post: &Pipeline{
+			Policy: policy,
+			Handlers: handlers,
+		},
+	}
+}
+
+func PatchEndpoint(route string, handlers ...Handler) Endpoint {
+	return Endpoint{
+		Route: route,
+		Patch: &Pipeline{
+			Handlers: handlers,
+		},
+	}
+}
+
+func PatchEndpointWithPolicy(route string, policy Policy, handlers ...Handler) Endpoint {
+	return Endpoint{
+		Route: route,
+		Patch: &Pipeline{
+			Policy: policy,
+			Handlers: handlers,
+		},
+	}
+}
+
+func DeleteEndpoint(route string, handlers ...Handler) Endpoint {
+	return Endpoint{
+		Route: route,
+		Delete: &Pipeline{
+			Handlers: handlers,
+		},
+	}
+}
+
+func DeleteEndpointWithPolicy(route string, policy Policy, handlers ...Handler) Endpoint {
+	return Endpoint{
+		Route: route,
+		Delete: &Pipeline{
+			Policy: policy,
+			Handlers: handlers,
+		},
+	}
+}
