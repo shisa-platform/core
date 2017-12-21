2fec0e13dc6562fa3caa3f68a83e9307d13795c9
Tom Hadley <tom.hadley@percolate.com>
2017-12-21 14:32:43 -0500

    move SiteUrl to struct, use named types for except/extractor and add better error messaging

--- middleware/csrf.go
+++ middleware/csrf.go
@@ -7,6 +7,7 @@ import (
 	"net/url"
 	"strings"
 
+	"github.com/percolate/shisa/authn"
 	"github.com/percolate/shisa/context"
 	"github.com/percolate/shisa/env"
 	"github.com/percolate/shisa/service"
@@ -17,103 +18,116 @@ const (
 	defaultTokenLength = 32
 )
 
+type ExemptChecker func(context.Context, *service.Request) bool
+
 type CSRFProtector struct {
-	CookieName   string
-	TokenLength  int
-	ExtractToken func(*service.Request) ([]byte, error)
-	Exempt       func(context.Context, *service.Request) bool
-	ErrorHandler service.ErrorHandler
+	SiteURL       string
+	CookieName    string
+	TokenLength   int
+	ExtractToken  authn.TokenExtractor
+	ExemptChecker ExemptChecker
+	ErrorHandler  service.ErrorHandler
 }
 
-func (p *CSRFProtector) Service(c context.Context, r *service.Request) service.Response {
-	if p.ErrorHandler == nil {
-		p.ErrorHandler = p.defaultErrorHandler
+func (m *CSRFProtector) Service(c context.Context, r *service.Request) service.Response {
+	if m.ErrorHandler == nil {
+		m.ErrorHandler = m.defaultErrorHandler
 	}
 
-	if p.CookieName == "" {
-		p.CookieName = defaultCookieName
+	if m.ExemptChecker == nil {
+		m.ExemptChecker = m.defaultExemptChecker
 	}
 
-	if p.TokenLength == 0 {
-		p.TokenLength = defaultTokenLength
+	if m.CookieName == "" {
+		m.CookieName = defaultCookieName
 	}
 
-	if p.Exempt(c, r) {
+	if m.TokenLength == 0 {
+		m.TokenLength = defaultTokenLength
+	}
+
+	if m.ExemptChecker(c, r) {
 		return nil
 	}
 
 	var err error
 
-	siteUrl, merr := env.Get("SITE_URL")
-	if merr != nil {
-		merr := merry.Wrap(merr)
-		return p.ErrorHandler(c, r, merr)
-	}
-	expected, err := url.Parse(siteUrl)
+	expected, err := url.Parse(m.SiteURL)
 	if err != nil {
-		merr := merry.Errorf("SiteURL %q is not a URL", siteUrl)
-		return p.ErrorHandler(c, r, merr)
+		merr := merry.Errorf("failure parsing siteurl: %f", m.SiteURL)
+		merr = merr.WithUserMessagef("SiteURL %f is not a URL", m.SiteURL)
+		return m.ErrorHandler(c, r, merr)
 	}
 	if values, exists := r.Header["Origin"]; exists {
 		actual, err := url.Parse(values[0])
 		if err != nil {
-			merr := merry.Errorf("presented Origin %q is not a URL", values[0])
+			merr := merry.Errorf("failure parsing Origin header: %f", values[0])
+			merr = merr.WithUserMessagef("Origin header is not a URL: %f", values[0])
 			merr = merr.WithHTTPCode(http.StatusInternalServerError)
-			return p.ErrorHandler(c, r, merr)
+			return m.ErrorHandler(c, r, merr)
 		}
 		if !sameOrigin(expected, actual) {
-			merr := merry.Errorf("presented Origin %q invalid, expected: %q", values[0], siteUrl)
+			merr := merry.Errorf("presented Origin %f invalid, expected: %f", values[0], m.SiteURL)
+			merr = merr.WithUserMessagef("Invalid Origin header %f, expected: %f", values[0], m.SiteURL)
 			merr = merr.WithHTTPCode(http.StatusInternalServerError)
-			return p.ErrorHandler(c, r, merr)
+			return m.ErrorHandler(c, r, merr)
 		}
 	} else if values, exists := r.Header["Referer"]; exists {
 		actual, err := url.Parse(values[0])
 		if err != nil {
-			merr := merry.Errorf("presented Referer %q is not a URL", values[0])
+			merr := merry.Errorf("failure parsing Referrer header: %f", values[0])
+			merr = merr.WithUserMessagef("Referrer header is not a URL: %f", values[0])
 			merr = merr.WithHTTPCode(http.StatusInternalServerError)
-			return p.ErrorHandler(c, r, merr)
+			return m.ErrorHandler(c, r, merr)
 		}
 		if !sameOrigin(expected, actual) {
-			merr := merry.Errorf("presented Referer %q invalid, expected: %q", values[0], siteUrl)
+			merr := merry.Errorf("presented Referrer %f invalid, expected: %f", values[0], m.SiteURL)
+			merr = merr.WithUserMessagef("Invalid Referrer header %f, expected: %f", values[0], m.SiteURL)
 			merr = merr.WithHTTPCode(http.StatusInternalServerError)
-			return p.ErrorHandler(c, r, merr)
+			return m.ErrorHandler(c, r, merr)
 		}
 	} else {
 		merr := merry.New("no Origin or Referer header presented")
+		merr = merr.WithUserMessage("Missing Origin or Referrer header")
 		merr = merr.WithHTTPCode(http.StatusForbidden)
-		return p.ErrorHandler(c, r, merr)
+		return m.ErrorHandler(c, r, merr)
 	}
 
-	cookie, err := r.Cookie(p.CookieName)
+	cookie, err := r.Cookie(m.CookieName)
 	if err != nil {
 		merr := merry.Wrap(err)
 		merr = merr.WithMessage("no CSRF cookie presented")
+		merr = merr.WithUserMessage("Missing Origin or Referrer header")
 		merr = merr.WithHTTPCode(http.StatusForbidden)
-		return p.ErrorHandler(c, r, merr)
+		return m.ErrorHandler(c, r, merr)
 	}
-	if len(cookie.Value) != p.TokenLength {
-		merr := merry.New("invalid CSRF cookie presented")
+	if len(cookie.Value) != m.TokenLength {
+		merr := merry.New("invalid CSRF cookie")
+		merr = merry.WithUserMessage("Invalid CSRF cookie presented")
 		merr = merr.WithHTTPCode(http.StatusForbidden)
-		return p.ErrorHandler(c, r, merr)
+		return m.ErrorHandler(c, r, merr)
 	}
 
-	token, err := p.ExtractToken(r)
+	token, err := m.ExtractToken(r)
 	if err != nil {
 		merr := merry.Wrap(err)
 		merr = merr.WithMessage("invalid CSRF token")
+		merr = merr.WithUserMessage("Invalid CSRF token")
 		merr = merr.WithHTTPCode(http.StatusForbidden)
-		return p.ErrorHandler(c, r, merr)
+		return m.ErrorHandler(c, r, merr)
 	}
-	if len(token) != p.TokenLength {
+	if len(token) != m.TokenLength {
 		merr := merry.New("invalid CSRF token")
+		merr = merr.WithUserMessage("Invalid CSRF token")
 		merr = merr.WithHTTPCode(http.StatusForbidden)
-		return p.ErrorHandler(c, r, merr)
+		return m.ErrorHandler(c, r, merr)
 	}
 
 	if subtle.ConstantTimeCompare([]byte(cookie.Value), []byte(token)) != 1 {
 		merr := merry.New("invalid CSRF token")
+		merr = merr.WithUserMessage("Invalid CSRF token")
 		merr = merr.WithHTTPCode(http.StatusForbidden)
-		return p.ErrorHandler(c, r, merr)
+		return m.ErrorHandler(c, r, merr)
 	}
 
 	return nil
@@ -128,4 +142,8 @@ func sameOrigin(expected, actual *url.URL) bool {
 
 func (m *CSRFProtector) defaultErrorHandler(ctx context.Context, r *service.Request, err merry.Error) service.Response {
 	return service.NewEmpty(merry.HTTPCode(err))
+}
+
+func (m *CSRFProtector) defaultExemptChecker(c context.Context, r *service.Request) bool {
+	return false
 }
