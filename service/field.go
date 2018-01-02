654a1884f67fec7332387336d9c6ed6d8c02ea4e
kevin birch <kmb@percolate.com>
2018-01-02 18:57:01 -0500

    add doc comments

--- service/field.go
+++ service/field.go
@@ -5,18 +5,28 @@ import (
 	"time"
 
 	"github.com/ansel1/merry"
+	"go.uber.org/multierr"
 )
 
+// Validator ensures that the given strings all meet certain
+// criteria.
 type Validator func([]string) merry.Error
 
+// Field is the schema to validate a query parameter or request
+// body.
+// Either `Name` or `Regex` _should_ be provided.
+// Providing `Default` *requires* `Name`.
+// If `Default` is provided and a matching input is not presented
+// then a syntheitic value will be created.
 type Field struct {
-	Name      string
-	Regex     *regexp.Regexp
-	Default   string
-	Validator Validator
-	Required  bool
+	Name      string         // Match keys by exact value
+	Regex     *regexp.Regexp // Match keys by pattern
+	Default   string         // Default for Name when no input
+	Validator Validator      // Optional validator of value(s)
+	Required  bool           // Is this input mandatory?
 }
 
+// Match returns true if the given key name is for this Field
 func (f *Field) Match(name string) bool {
 	if f.Regex != nil {
 		return f.Regex.MatchString(name)
@@ -25,6 +35,8 @@ func (f *Field) Match(name string) bool {
 	return f.Name == name
 }
 
+// Validate returns an error if all input values don't meet the
+// criteria of `Field.Validator`.
 func (f *Field) Validate(value []string) merry.Error {
 	if f.Validator != nil {
 		return f.Validator(value)
@@ -33,26 +45,30 @@ func (f *Field) Validate(value []string) merry.Error {
 	return nil
 }
 
-type StringMatchValidator struct {
+// FixedStringValidator enforces that all input values match a
+// fixed string
+type FixedStringValidator struct {
 	Target string
 }
 
-func (v StringMatchValidator) Validate(values []string) merry.Error {
-	if len(values) != 1 {
-		return merry.New("expected 1 value").WithUserMessage("too many values")
-	}
-	if v.Target != values[0] {
-		return merry.New("unexpected value").WithUserMessage("unsupported value").WithValue("value", values[0])
+func (v FixedStringValidator) Validate(values []string) merry.Error {
+	for _, value := range values {
+		if v.Target != value {
+			return merry.New("unexpected value").WithUserMessage("Value is not allowed").WithValue("value", value)
+		}
 	}
 
 	return nil
 }
 
+// StringSliceValidator enforces that all input values match at
+// least one of the given strings
 type StringSliceValidator struct {
 	Target []string
 }
 
 func (v StringSliceValidator) Validate(values []string) merry.Error {
+	var err error
 	for _, value := range values {
 		var found bool
 		for _, t := range v.Target {
@@ -62,11 +78,12 @@ func (v StringSliceValidator) Validate(values []string) merry.Error {
 			}
 		}
 		if !found {
-
+			err1 := merry.New("unexpected value").WithUserMessage("Value is not allowed").WithValue("value", value)
+			multierr.Combine(err, err1)
 		}
 	}
 
-	return nil
+	return merry.Wrap(err)
 }
 
 type StringValidator struct {
