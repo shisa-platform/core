27da195b2e20ae817102ee5a31adb51557fbf13a
kevin birch <kmb@percolate.com>
2018-01-02 18:41:42 -0500

    add query parameter coercion tests

--- service/field.go
+++ service/field.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"regexp"
+	"time"
 
 	"github.com/ansel1/merry"
 )
@@ -32,7 +33,71 @@ func (f *Field) Validate(value []string) merry.Error {
 	return nil
 }
 
+type StringMatchValidator struct {
+	Target string
+}
+
+func (v StringMatchValidator) Validate(values []string) merry.Error {
+	if len(values) != 1 {
+		return merry.New("expected 1 value").WithUserMessage("too many values")
+	}
+	if v.Target != values[0] {
+		return merry.New("unexpected value").WithUserMessage("unsupported value").WithValue("value", values[0])
+	}
+
+	return nil
+}
+
+type StringSliceValidator struct {
+	Target []string
+}
+
+func (v StringSliceValidator) Validate(values []string) merry.Error {
+	for _, value := range values {
+		var found bool
+		for _, t := range v.Target {
+			if value == t {
+				found = true
+				break
+			}
+		}
+		if !found {
+
+		}
+	}
+
+	return nil
+}
+
+type StringValidator struct {
+	MaxLen uint
+	MinLen uint
+}
+
+type IntValidator struct {
+	Min int
+	Max int
+}
+
 type UintValidator struct {
 	Min uint
 	Max uint
+}
+
+func (v UintValidator) Validate(values []string) merry.Error {
+	return nil
+}
+
+type BoolValidator struct {
+	Expected bool
+}
+
+type TimestampValidator struct {
+	Format string
+	Min    time.Time
+	Max    time.Time
+}
+
+type RegexValidator struct {
+	Regex *regexp.Regexp
 }
--- service/queryparam.go
+++ service/queryparam.go
@@ -1,8 +1,12 @@
 package service
 
 import (
-	"github.com/ansel1/merry"
+	"encoding/csv"
+	"io"
+	"strconv"
 	"time"
+
+	"github.com/ansel1/merry"
 )
 
 // QueryParameter is a single query parameter passed to an
@@ -15,102 +19,254 @@ type QueryParameter struct {
 	Unknown bool     // the pair was not matched by a field
 }
 
-func (p *QueryParameter) CSV(t *[]string) merry.Error {
+type sliceReader struct {
+	i, j   int64
+	values []string
+}
+
+func (r *sliceReader) Read(p []byte) (n int, err error) {
+	if r.i == int64(len(r.values)-1) && r.j >= int64(len(r.values[r.i])) {
+		return 0, io.EOF
+	}
+
+	if r.j >= int64(len(r.values[r.i])) {
+		p[n] = 0x0A
+		n++
+		r.i++
+		r.j = 0
+	}
+
+	n += copy(p[n:], r.values[r.i][r.j:])
+	r.j += int64(n)
+
+	return
+}
+
+func (p *QueryParameter) CSV(v *[]string) merry.Error {
+	reader := csv.NewReader(&sliceReader{values: p.Values})
+	records, err := reader.ReadAll()
+	if err != nil {
+		return merry.Wrap(err)
+	}
+	for _, fields := range records {
+		*v = append(*v, fields...)
+	}
+
 	return nil
 }
 
-func (p *QueryParameter) Bool(t *bool) merry.Error {
+func (p *QueryParameter) Bool(v *bool) merry.Error {
+	b, err := strconv.ParseBool(p.Values[0])
+	*v = b
+	return merry.Wrap(err)
+}
+
+func (p *QueryParameter) BoolSlice(v *[]bool) merry.Error {
+	for _, value := range p.Values {
+		b, err := strconv.ParseBool(value)
+		if err != nil {
+			return merry.Wrap(err)
+		}
+		*v = append(*v, b)
+	}
+
 	return nil
 }
 
-func (p *QueryParameter) BoolSlice(t *[]bool) merry.Error {
+func (p *QueryParameter) Time(v *time.Time, format string) merry.Error {
+	t, err := time.Parse(format, p.Values[0])
+	*v = t
+	return merry.Wrap(err)
+}
+
+func (p *QueryParameter) TimeSlice(v *[]time.Time, format string) merry.Error {
+	for _, value := range p.Values {
+		t, err := time.Parse(format, value)
+		if err != nil {
+			return merry.Wrap(err)
+		}
+		*v = append(*v, t)
+	}
+
 	return nil
 }
 
-func (p *QueryParameter) Time(t *time.Time, format string) merry.Error {
+func (p *QueryParameter) Int8(v *int8) merry.Error {
+	i, err := strconv.ParseInt(p.Values[0], 10, 8)
+	*v = int8(i)
+	return merry.Wrap(err)
+}
+
+func (p *QueryParameter) Int8Slice(v *[]int8) merry.Error {
+	for _, value := range p.Values {
+		i, err := strconv.ParseInt(value, 10, 8)
+		if err != nil {
+			return merry.Wrap(err)
+		}
+		*v = append(*v, int8(i))
+	}
+
 	return nil
 }
 
-func (p *QueryParameter) TimeSlice(t *[]time.Time, format string) merry.Error {
+func (p *QueryParameter) Int16(v *int16) merry.Error {
+	i, err := strconv.ParseInt(p.Values[0], 10, 16)
+	*v = int16(i)
+	return merry.Wrap(err)
+}
+
+func (p *QueryParameter) Int16Slice(v *[]int16) merry.Error {
+	for _, value := range p.Values {
+		i, err := strconv.ParseInt(value, 10, 16)
+		if err != nil {
+			return merry.Wrap(err)
+		}
+		*v = append(*v, int16(i))
+	}
+
 	return nil
 }
 
-func (p *QueryParameter) Uint8(t *uint8) merry.Error {
+func (p *QueryParameter) Int32(v *int32) merry.Error {
+	i, err := strconv.ParseInt(p.Values[0], 10, 32)
+	*v = int32(i)
+	return merry.Wrap(err)
+}
+
+func (p *QueryParameter) Int32Slice(v *[]int32) merry.Error {
+	for _, value := range p.Values {
+		i, err := strconv.ParseInt(value, 10, 32)
+		if err != nil {
+			return merry.Wrap(err)
+		}
+		*v = append(*v, int32(i))
+	}
+
 	return nil
 }
 
-func (p *QueryParameter) Uint8Slice(t *[]uint8) merry.Error {
+func (p *QueryParameter) Int64(v *int64) merry.Error {
+	i, err := strconv.ParseInt(p.Values[0], 10, 64)
+	*v = int64(i)
+	return merry.Wrap(err)
+}
+
+func (p *QueryParameter) Int64Slice(v *[]int64) merry.Error {
+	for _, value := range p.Values {
+		i, err := strconv.ParseInt(value, 10, 64)
+		if err != nil {
+			return merry.Wrap(err)
+		}
+		*v = append(*v, int64(i))
+	}
+
 	return nil
 }
 
-func (p *QueryParameter) Uint16(t *uint16) merry.Error {
+func (p *QueryParameter) Int(v *int) merry.Error {
+	i, err := strconv.Atoi(p.Values[0])
+	*v = i
+	return merry.Wrap(err)
+}
+
+func (p *QueryParameter) IntSlice(v *[]int) merry.Error {
+	for _, value := range p.Values {
+		i, err := strconv.Atoi(value)
+		if err != nil {
+			return merry.Wrap(err)
+		}
+		*v = append(*v, i)
+	}
+
 	return nil
 }
 
-func (p *QueryParameter) Uint16Slice(t *[]uint16) merry.Error {
+func (p *QueryParameter) Uint8(v *uint8) merry.Error {
+	i, err := strconv.ParseUint(p.Values[0], 10, 8)
+	*v = uint8(i)
+	return merry.Wrap(err)
+}
+
+func (p *QueryParameter) Uint8Slice(v *[]uint8) merry.Error {
+	for _, value := range p.Values {
+		i, err := strconv.ParseUint(value, 10, 8)
+		if err != nil {
+			return merry.Wrap(err)
+		}
+		*v = append(*v, uint8(i))
+	}
+
 	return nil
 }
 
-func (p *QueryParameter) Uint32(t *uint32) merry.Error {
+func (p *QueryParameter) Uint16(v *uint16) merry.Error {
+	i, err := strconv.ParseUint(p.Values[0], 10, 16)
+	*v = uint16(i)
+	return merry.Wrap(err)
+}
+
+func (p *QueryParameter) Uint16Slice(v *[]uint16) merry.Error {
+	for _, value := range p.Values {
+		i, err := strconv.ParseUint(value, 10, 16)
+		if err != nil {
+			return merry.Wrap(err)
+		}
+		*v = append(*v, uint16(i))
+	}
+
 	return nil
 }
 
-func (p *QueryParameter) Uint32Slice(t *[]uint32) merry.Error {
+func (p *QueryParameter) Uint32(v *uint32) merry.Error {
+	i, err := strconv.ParseUint(p.Values[0], 10, 32)
+	*v = uint32(i)
+	return merry.Wrap(err)
+}
+
+func (p *QueryParameter) Uint32Slice(v *[]uint32) merry.Error {
+	for _, value := range p.Values {
+		i, err := strconv.ParseUint(value, 10, 32)
+		if err != nil {
+			return merry.Wrap(err)
+		}
+		*v = append(*v, uint32(i))
+	}
+
 	return nil
 }
 
-func (p *QueryParameter) Uint64(t *uint64) merry.Error {
+func (p *QueryParameter) Uint64(v *uint64) merry.Error {
+	i, err := strconv.ParseUint(p.Values[0], 10, 64)
+	*v = uint64(i)
+	return merry.Wrap(err)
+}
+
+func (p *QueryParameter) Uint64Slice(v *[]uint64) merry.Error {
+	for _, value := range p.Values {
+		i, err := strconv.ParseUint(value, 10, 64)
+		if err != nil {
+			return merry.Wrap(err)
+		}
+		*v = append(*v, uint64(i))
+	}
+
 	return nil
 }
 
-func (p *QueryParameter) Uint64Slice(t *[]uint64) merry.Error {
-	return nil
+func (p *QueryParameter) Uint(v *uint) merry.Error {
+	i, err := strconv.ParseUint(p.Values[0], 10, 0)
+	*v = uint(i)
+	return merry.Wrap(err)
 }
 
-func (p *QueryParameter) Uint(t *uint) merry.Error {
-	return nil
-}
+func (p *QueryParameter) UintSlice(v *[]uint) merry.Error {
+	for _, value := range p.Values {
+		i, err := strconv.ParseUint(value, 10, 0)
+		if err != nil {
+			return merry.Wrap(err)
+		}
+		*v = append(*v, uint(i))
+	}
 
-func (p *QueryParameter) UintSlice(t *[]uint) merry.Error {
-	return nil
-}
-
-func (p *QueryParameter) Int8(t *int8) merry.Error {
-	return nil
-}
-
-func (p *QueryParameter) Int8Slice(t *[]int8) merry.Error {
-	return nil
-}
-
-func (p *QueryParameter) Int16(t *int16) merry.Error {
-	return nil
-}
-
-func (p *QueryParameter) Int16Slice(t *[]int16) merry.Error {
-	return nil
-}
-
-func (p *QueryParameter) Int32(t *int32) merry.Error {
-	return nil
-}
-
-func (p *QueryParameter) Int32Slice(t *[]int32) merry.Error {
-	return nil
-}
-
-func (p *QueryParameter) Int64(t *int64) merry.Error {
-	return nil
-}
-
-func (p *QueryParameter) Int64Slice(t *[]int64) merry.Error {
-	return nil
-}
-
-func (p *QueryParameter) Int(t *int) merry.Error {
-	return nil
-}
-
-func (p *QueryParameter) IntSlice(t *[]int) merry.Error {
 	return nil
 }
