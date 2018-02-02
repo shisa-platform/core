5f4df5240ba1afbfd3ce85f425d62bcb5191067c
kevin birch <kmb@percolate.com>
2018-02-02 18:58:38 -0500

    simplify example gw main

--- examples/gw/main.go
+++ examples/gw/main.go
@@ -1,27 +1,18 @@
 package main
 
 import (
-	"expvar"
 	"flag"
 	"log"
-	"time"
 
 	"go.uber.org/zap"
 )
 
 const (
-	timeFormat            = "2006-01-02T15:04:05+00:00"
 	goodbyeServiceAddrEnv = "GOODBYE_SERVICE_ADDR"
 	idpServiceAddrEnv     = "IDP_SERVICE_ADDR"
-	gateway               = expvar.NewMap("gateway")
 )
 
 func main() {
-	now := time.Now().UTC().Format(timeFormat)
-	startTime := new(expvar.String)
-	startTime.Set(now)
-	gateway.Set("start-time", startTime)
-
 	addr := flag.String("addr", "", "gateway service address")
 	debugAddr := flag.String("debugaddr", ":0", "debug service address")
 	healthcheckAddr := flag.String("healthcheckaddr", ":0", "healthcheck service address")
@@ -35,5 +26,5 @@ func main() {
 
 	defer logger.Sync()
 
-	serve(addr, debugAddr, healthcheckAddr)
+	serve(logger, *addr, *debugAddr, *healthcheckAddr)
 }
