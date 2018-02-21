7288a6a8472a2917867817c92d3a99e71fc5b56f
kevin birch <kmb@percolate.com>
2018-02-21 15:47:36 +0900

    refactor `ResponseInterceptor`
    - remove logging
    - add `ResponseSnapshot` support on `Flush` method
    - add `WriteResponse` method
    - add default status code value
    - update goodbye example service

--- httpx/interceptor.go
+++ httpx/interceptor.go
@@ -4,20 +4,13 @@ import (
 	"net/http"
 	"time"
 
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
-
-	"github.com/percolate/shisa/context"
 	"github.com/percolate/shisa/service"
 )
 
 // ResponseInterceptor implements `http.ResponseWriter` to capture
-// and log the response sent to a user agent.
-// Use it to wrap a standard  `http.ResponseWriter` and log the
-// request to a logger named "request" at the `Info` level.
+// the outgoing response status code and size.
 type ResponseInterceptor struct {
 	http.ResponseWriter
-	logger *zap.Logger
 	start  time.Time
 	status int
 	size   int
@@ -39,42 +32,36 @@ func (i *ResponseInterceptor) WriteHeader(status int) {
 	i.ResponseWriter.WriteHeader(status)
 }
 
-// Flush logs the request at the `Info` level.
-// No logging is peformed the `Info` level is not configured.  If
-// the underlying writer implements `http.Flusher` then the
-// `Flush` method will be called.
-func (i *ResponseInterceptor) Flush(ctx context.Context, request *service.Request) {
-	if ce := i.logger.Check(zap.InfoLevel, "request"); ce != nil {
-		end := time.Now().UTC()
-		elapsed := end.Sub(i.start)
-		if i.status == 0 {
-			i.status = http.StatusOK
-		}
-		fs := make([]zapcore.Field, 9, 10)
-		fs[0] = zap.String("request-id", ctx.RequestID())
-		fs[1] = zap.String("client-ip-address", request.ClientIP())
-		fs[2] = zap.String("method", request.Method)
-		fs[3] = zap.String("uri", request.URL.RequestURI())
-		fs[4] = zap.Int("status-code", i.status)
-		fs[5] = zap.Int("response-size", i.size)
-		fs[6] = zap.String("user-agent", request.UserAgent())
-		fs[7] = zap.Time("start", i.start)
-		fs[8] = zap.Duration("elapsed", elapsed)
-		if u := ctx.Actor(); u != nil {
-			fs = append(fs, zap.String("user-id", u.ID()))
-		}
-		ce.Write(fs...)
-	}
+// WriteResponse serializes a response instance
+// The `WriteHeader` method will *always* be called with the
+// value of `Response.StatusCode()` so it is not safe to use the
+// `ResponseWriter` methods of this instance after calling this
+// method.
+// Any error returned from `Response.Serialize` will be returned.
+func (i *ResponseInterceptor) WriteResponse(response service.Response) error {
+	return WriteResponse(i, response)
+}
 
+// Flush attempts to call the `Flush` method on the underlying
+// `ResponseWriter`, if it implments the `http.Flusher` interface.
+func (i *ResponseInterceptor) Flush() ResponseSnapshot {
 	if f, ok := i.ResponseWriter.(http.Flusher); ok {
 		f.Flush()
 	}
+
+	return ResponseSnapshot{
+		StatusCode: i.status,
+		Size:       i.size,
+		Start:      i.start,
+		Elapsed:    time.Now().UTC().Sub(i.start),
+		Metrics:    make(map[string]time.Duration),
+	}
 }
 
-func NewInterceptor(w http.ResponseWriter, logger *zap.Logger) *ResponseInterceptor {
+func NewInterceptor(w http.ResponseWriter) *ResponseInterceptor {
 	return &ResponseInterceptor{
 		ResponseWriter: w,
-		logger:         logger,
+		status:         http.StatusOK,
 		start:          time.Now().UTC(),
 	}
 }
