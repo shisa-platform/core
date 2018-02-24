46c6c1677cfc6c7b2215739c452878d089216243
kevin birch <kmb@percolate.com>
2018-02-24 12:03:52 +0900

    use request/context pools in auxiliary servers

--- auxiliary/http.go
+++ auxiliary/http.go
@@ -212,8 +212,11 @@ func (s *HTTPServer) Shutdown(timeout time.Duration) error {
 func (s *HTTPServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	ri := httpx.NewInterceptor(w)
 
-	ctx := context.New(r.Context())
-	request := &httpx.Request{Request: r}
+	ctx := context.Get(r.Context())
+	defer context.Put(ctx)
+
+	request := httpx.GetRequest(r)
+	defer httpx.PutRequest(request)
 
 	requestID, idErr := s.RequestIDGenerator(ctx, request)
 	if idErr != nil {
