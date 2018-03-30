6cd7fb8ba7deca9f01ffc092451de41cd60a9bfb
kevin birch <kmb@percolate.com>
2018-03-30 17:36:41 -0400

    add errorx package

---
+++ errorx/panic.go
@@ -0,0 +1,40 @@
+package errorx
+
+import (
+	"fmt"
+
+	"github.com/ansel1/merry"
+)
+
+const (
+	sentinel = "panic"
+)
+
+func Panic(arg interface{}, message string) (err merry.Error) {
+	if err1, ok := arg.(error); ok {
+		err = merry.Prepend(err1, message)
+	} else {
+		err = merry.Errorf("%s: \"%v\"", message, arg)
+	}
+
+	err = err.WithValue(sentinel, true)
+
+	return
+}
+
+func Panicf(arg interface{}, format string, args ...interface{}) merry.Error {
+	return Panic(arg, fmt.Sprintf(format, args...))
+}
+
+func IsPanic(err merry.Error) bool {
+	value := merry.Value(err, sentinel)
+	if value == nil {
+		return false
+	}
+
+	if panic, ok := value.(bool); ok && panic {
+		return true
+	}
+
+	return false
+}
