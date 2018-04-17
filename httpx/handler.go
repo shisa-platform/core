d6c69bf28209eb47b4a609af38622ecc780fdf7b
kevin birch <kmb@percolate.com>
2018-04-17 18:33:07 -0400

    implement `AdaptStandardHandler`

--- httpx/handler.go
+++ httpx/handler.go
@@ -1,6 +1,10 @@
 package httpx
 
 import (
+	"bytes"
+	"io"
+	"net/http"
+
 	"github.com/ansel1/merry"
 
 	"github.com/percolate/shisa/context"
@@ -32,4 +36,67 @@ func handlerRecovery(exception *merry.Error) {
 	}
 
 	*exception = errorx.CapturePanic(arg, "panic in handler")
+}
+
+type responseBuffer struct {
+	code    int
+	headers http.Header
+	body    *bytes.Buffer
+	written bool
+}
+
+func (r *responseBuffer) StatusCode() int {
+	return r.code
+}
+
+func (r *responseBuffer) Headers() http.Header {
+	return r.headers
+}
+
+func (r *responseBuffer) Trailers() http.Header {
+	return nil
+}
+
+func (r *responseBuffer) Err() error {
+	return nil
+}
+
+func (r *responseBuffer) Serialize(w io.Writer) merry.Error {
+	_, err := io.Copy(w, r.body)
+
+	return merry.Wrap(err)
+}
+
+func (r *responseBuffer) Header() http.Header {
+	return r.headers
+}
+
+func (r *responseBuffer) Write(bs []byte) (int, error) {
+	r.written = true
+	return r.body.Write(bs)
+}
+
+func (r *responseBuffer) WriteHeader(statusCode int) {
+	r.written = true
+	r.code = statusCode
+}
+
+// AdaptStandardHandler allows an http.Hander to be used as an
+// httpx.Handler
+func AdaptStandardHandler(handler http.Handler) Handler {
+	return func(ctx context.Context, request *Request) Response {
+		w := responseBuffer{
+			code: http.StatusOK,
+			headers: make(http.Header),
+			body: new(bytes.Buffer),
+		}
+		r := request.Request.WithContext(ctx)
+
+		handler.ServeHTTP(&w, r)
+		if w.written {
+			return &w
+		}
+
+		return nil
+	}
 }
